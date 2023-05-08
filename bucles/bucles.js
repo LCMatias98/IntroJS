9816537e5f6d593b4bdab868ca00ab5f54add797
LCMatias98 <correalucasmatias98@gmail.com>
2023-05-07 22:37:19 -0300

    Se agregan ejercicios de Bucles

--- bucles/bucles.js
+++ bucles/bucles.js
@@ -48,7 +48,7 @@ while(bucle){
 /* 
 Ejercicio 4 y 5  */
 
-let numeroIngresado = parseInt(prompt("ingrese un numero para encontrar sus primos"))
+/* let numeroIngresado = parseInt(prompt("ingrese un numero para encontrar sus primos"))
 let numerosPrimos = []
 
 for(let i=0; i <= numeroIngresado; i++){
@@ -71,4 +71,96 @@ if (numerosPrimos.length > 2){
     console.log(`el numero es primo`)
 }
 
-console.log(numerosPrimos)
+console.log(numerosPrimos) */
+
+/* Ejercicio 6 */
+
+/* const arrayNumeros = [1,2,3,4,5,6,7,8,9,10]
+console.log(arrayNumeros)
+for(let i=0; i <= arrayNumeros.length; i++){
+    console.log(arrayNumeros[i])
+} */
+/* 
+Ejercicio 7 */
+/* 
+const arrayNumeros = [1,2,3,4,5,6,7,8,9,10]
+
+for(let i=0; i <= arrayNumeros.length-1; i++){
+    console.log(arrayNumeros[i]*2) 
+
+} */
+
+// Ejercicio 8
+/* 
+const familia = [
+                {nombre: "Dante",parentesco:"El Padre",edad:50,profesion:"Enfermero"},
+                {nombre: "Isabel",parentesco:"La Madre",edad:50,profesion:"Enfermera"},
+                {nombre: "Leonardo",parentesco:"El Hmno",edad:50,profesion:"Mecanico"},
+                {nombre: "Nicolas",parentesco:"El Hmno",edad:50,profesion:"Estudiante"},
+                {nombre: "Lucas",parentesco:"El Hmno",edad:50,profesion:"Programador Full Stack Java"}
+]
+// console.log(familia)
+
+for (let persona of familia){
+    console.log(`Hola soy ${persona.nombre}, en mi familia soy ${persona.parentesco} tengo ${persona.edad} años de edad y mi profesion es ${persona.profesion}`)
+} */
+
+ // Ejercicio 9
+
+/*  const arrayNumeros = [1,2,3,4,5,6,7,8,9,10]
+
+ for(let i =0;i < arrayNumeros.length ;i++ ){
+    if(arrayNumeros[i] % 2 !=0){
+        console.log(arrayNumeros[i])
+    }
+ } */
+
+ // Ejercicio 10
+/* 
+ Realizar un programa que permita la entrada de números y calcule la suma de los números pares por
+un lado y los impares por otro, el ingreso de dato finaliza cuando el usuario ingresa un 0.  */
+
+/* let numero = parseInt(prompt("Ingrese un numero"))
+let numerosIngresados = []
+
+while(numero){
+    numerosIngresados.push(numero)
+    numero = parseInt(prompt("Ingrese numero"))
+}
+
+console.log(numerosIngresados)
+
+
+let numerosImpares = 0
+let numerosPares = 0
+for(let i =0;i < numerosIngresados.length ;i++ ){
+    if(numerosIngresados[i] % 2 !=0){
+    numerosImpares +=  numerosIngresados[i]
+    }else if(numerosIngresados[i] % 2 ===0){
+        numerosPares += numerosIngresados[i]
+    }
+}
+console.log(`La suma de los numeros impares es: ${numerosImpares}`)
+console.log(`La suma de los numeros pares es: ${numerosPares}`) */
+
+
+// Ejercicio 11 y 12
+
+const arrayNumeros = [10,8,400,50,60,85,100,54,80,90]
+
+let numeroMasAlto = 0
+let numeroMasPequeño = arrayNumeros[0]
+
+for (let numero of arrayNumeros){
+    if(numeroMasAlto  <  numero){
+    numeroMasAlto = numero
+    }
+}
+
+for (let i=0; i<arrayNumeros.length ; i++){
+    if(numeroMasPequeño  >  arrayNumeros[i]){
+    numeroMasPequeño=arrayNumeros[i]
+    }
+}
+console.log(numeroMasAlto)
+console.log(numeroMasPequeño)
